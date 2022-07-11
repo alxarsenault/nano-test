a112bfb78266cb9f09dbb170930e72fe2a2d3327
Alexandre Arsenault <aarsenault@landr.com>
2022-07-10 23:43:16 -0400

    Run all examples in ci

--- examples/assert/main.cpp
+++ examples/assert/main.cpp
@@ -10,7 +10,8 @@ inline void foo_throw(bool err) {
 TEST_CASE("Example", Exception, "Check exceptions", NANO_TEST_ABORT_ON_ERROR) {
   ASSERT_EXCEPTION(foo_throw(true), std::range_error);
   EXPECT_EXCEPTION(foo_throw(true), std::exception);
-  ASSERT_EXCEPTION(foo_throw(true), std::bad_cast);
+
+  // ASSERT_EXCEPTION(foo_throw(true), std::bad_cast);
 }
 } // namespace.
 
